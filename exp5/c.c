ad032c9e1ff370de727b11038709f6e45996835b
FightlolYes <yashpatil.uwu@gmail.com>
2023-02-21 09:57:27 +0530

    update in exp5

--- exp5/c.c
+++ exp5/c.c
@@ -6,7 +6,7 @@ int main() {
     char string[100] = "Yash@232107";
     int alp = 0, digit = 0, spchar = 0;
 
-    for(int i = 0; i > strlen(string); i++) {
+    for(int i = 1; i <= strlen(string); i++) {
         if(string[i] >= 65 && string[i] <= 90|| string[i]>=97 && string[i] <=122) {
             alp += 1;
         }
---
+++ exp5/d.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+
+    char username[10] = "Yash";
+    char inuser[10];
+
+    int num;
+    int facto = 1;
+
+    printf("Enter username: ");
+    gets(inuser);
+
+    if(strcmp(username, inuser) == 0) {
+        printf("Hello %s, enter a number: ", inuser);
+        scanf("%d", &num);
+
+        for(int i = 1; i<=num; i++) {
+            facto = facto*i;
+        }
+
+        printf("Factorial: %d", facto);
+
+    }
+
+    else {
+        printf("Wrong username!, try again");
+    }
+
+    return 0;
+}
