0873918f14ce9ea3b98a3f646cb5a0c2c2820171
FightlolYes <yashpatil.uwu@gmail.com>
2023-02-25 16:42:17 +0530

    finished exp7

--- exp7/a.c
+++ exp7/a.c
@@ -9,7 +9,35 @@ struct company {
 
 int main() {
 
+    struct company company[10];
 
+    for(int i = 0; i<10; i++) {
+
+        printf("Company %d name: ", i+1);
+        scanf("%s", &company[i].name);
+
+        printf("Company %d address: ", i+1);
+        scanf("%s", &company[i].address);
+
+        printf("Company %d phoneno: ", i+1);
+        scanf("%d", &company[i].phone);
+
+        printf("Company %d employee count: ", i+1);
+        scanf("%d", &company[i].noOfEmployee);
+                
+    }
+
+    for(int i = 0; i<10; i++) {
+
+        printf("Company %d name: %s\n", i+1, company[i].name);
+
+        printf("Company %d address: %s\n", i+1, company[i].address);
+
+        printf("Company %d phoneno: %d\n", i+1, company[i].phone);
+
+        printf("Company %d employee count: %d\n\n", i+1, company[i].noOfEmployee);
+                
+    }
 
     return 0;
 }
